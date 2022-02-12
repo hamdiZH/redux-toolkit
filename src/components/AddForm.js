83998a6c2bbea80c84ebb6b936037c7b1415055a
hamdiZH <hamdi.hijazi2018@gmail.com>
2022-02-12 11:57:06 +0200

    create book app sore

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -8,11 +8,11 @@ const Addform = () => {
         <form>
           <div className='form-group'>
             <label htmlFor='title'>Title</label>
-            <input type='text' className='form-control' id='title' required />
+            <input type='text' className='form-control' id='title' required/>
           </div>
           <div className='form-group'>
             <label htmlFor='price'>Price</label>
-            <input type='number' className='form-control' id='price' required />
+            <input type='number' className='form-control' id='price' required/>
           </div>
           <div className='form-group'>
             <label htmlFor='Description'>Description</label>
@@ -21,7 +21,7 @@ const Addform = () => {
               id='Description'
               rows='3'
               required
-            ></textarea>
+            />
           </div>
           <button type='submit' className='btn btn-primary'>
             Submit
---
+++ src/store/bookSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const bookSlice = createSlice({
+  name: "book",
+  initialState: { books: null },
+  reducers: {},
+})
+
+export default bookSlice.reducer;
