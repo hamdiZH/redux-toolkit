48a3d9af994197df5220afcc808ad1801925a533
hamdiZH <hamdi.hijazi2018@gmail.com>
2022-02-12 12:11:34 +0200

    developing first createAsyncThunc

--- src/store/bookSlice.js
+++ src/store/bookSlice.js
@@ -1,4 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+// CreateAsyncThunk takes two parameters: typePrefix and payloadCreator
+const getBooks = createAsyncThunk( 'book/getBooks', async (_, thunkAPI) => {
+    try {
+      const res = await fetch('http://localhost:5000/books');
+      const data = await res.json();
+      return data;
+
+    } catch (error) {
+      console.log(error)
+    }
+})
 
 const bookSlice = createSlice({
   name: "book",
